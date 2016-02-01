d5ce273742a1097e8e43e6d318cac2b98e39008a
Malvika <malvika@forbesmacbook.evl.uic.edu>
2016-02-01 16:07:35 -0600

    changed

--- get-pmc-xml/index.js
+++ get-pmc-xml/index.js
@@ -1,10 +1,25 @@
 var request = require('request');
 
 function getPmcXml(pmcId) {
-  request('http://www.google.com', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
+
+	var	pmcNum=pmcId.slice(3);
+	var url1='http://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:';
+	var url2 = '&metadataPrefix=pmc';
+	var url3 = url1 + pmcNum + url2;
+
+  // request(url3, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     console.log(body) // Show the HTML for the Google homepage.
+  //   }
+  // })
+
+  return new Promise(function(resolve) {
+  	request(url3, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	      // console.log(body) // Show the HTML for the Google homepage.
+	      resolve(body)
+	    }
+  	})
   })
 }
 
--- test.js
+++ test.js
@@ -1,4 +1,16 @@
 var getPmcXml = require('./get-pmc-xml');
 var assert = require('assert');
 
-getPmcXml("PMC3594181");
+var myPromise = getPmcXml("PMC3594181");
+
+// console.log(myPromise);
+// myPromise.then(function(blueberries) {
+// 	console.log(blueberries);
+// })
+
+myPromise.then(function(data) {
+	console.log(data)
+	return "whatever"
+}).then(function(data) {
+	console.log(data)
+})
